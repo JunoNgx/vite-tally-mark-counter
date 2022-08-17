f31c2e71b69cb22d8be22cb69a6b5d90f679caf3
Juno Nguyen <juno.ngx@gmail.com>
2022-08-17 19:24:58 +0700

    Arrange counters

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,40 @@
 <script lang="ts">
-    export let counter = 1;
+    import Counter from './lib/Counter.svelte'
+
+    let counter = 1;
 </script>
 
 <template lang="pug">
     .container
-        .child
+        .counter {counter}
+        - var i = 0
+        .tally-marks-container
+            while i < 20
+                Counter(serialNumber=i++)
 </template>
 
 <style lang="sass">
-    .child
-        background-color: cyan
-        width: 640px
-        height: 480px
+    .container
+        padding: 12px
+        border: 2px solid white
+        width: 720px
+        height: 720px
+        display: flex
+        flex-flow: column nowrap
+        justify-content: space-around
+
+        .counter
+            width: 100%
+            height: 64px
+            font-size: 4rem
+            display: grid
+            place-items: center center
+
+        .tally-marks-container
+            background-color: indianred
+            width: 100%
+            display: flex
+            flex-flow: row wrap
+            justify-content: space-between
+            gap: 10px
 </style>
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
-  }
+    export let serialNumber = 0
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+<template lang="pug">
+    .counter-container
+        .mark.mark-1
+        .mark.mark-2
+        .mark.mark-3
+        .mark.mark-4
+        .mark.mark-5
+</template>
+
+<style lang="sass">
+    .counter-container
+        border: 2px solid white
+        width: 150px
+        height: 75px
+        
+</style>
